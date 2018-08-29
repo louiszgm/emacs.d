44affc678f6c47c8eeba7268a194f545ac3f74c9
louiszgm <louiszgm@gmail.com>
2018-08-30 07:17:39 +0800

    setup org

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -2,7 +2,7 @@
 (setq blog-admin-backend-type 'nikola)
 (setq blog-admin-backend-path "~/blog")
 (setq blog-admin-backend-new-post-in-drafts t)
-(setq blog-admin-backend-nikola-executable "~/.venvs/nikola/bin/nikola") ;; path to nikola executable
+(setq blog-admin-backend-nikola-executable "~/dockerfiles/nikola/bin/nikola-docker") ;; path to nikola executable
 (setq blog-admin-backend-nikola-config-file "conf.py") ;; conf.py is default
 
 (require 'smartparens-config)
@@ -10,5 +10,9 @@
 (setq org2nikola-output-root-directory "~/blog")
 ;; use google code prettify (https://code.google.com/p/google-code-prettify/)
 (setq org2nikola-use-google-code-prettify t)
+(setq org2nikola-use-verbose-metadata t)
+
+
+
 
 (provide 'init-local)
